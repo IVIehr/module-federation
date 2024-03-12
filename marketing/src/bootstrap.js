172a56c5c24646416e5abc5b3425e16d6d433d9a
mehrnoush.navidi <mehr910093@gmail.com>
2024-03-12 13:47:33 +0330

    feat: add components to marketing app

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
 const mount = (el) => {
-  ReactDOM.render(<h1>hi from marketing-dev-root</h1>, el);
+  ReactDOM.render(<App />, el);
 };
 
-if(process.env.NODE_ENV === 'development'){
-    const devRoot = document.querySelector("#_marketing-dev-root")
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing-dev-root");
 
-    if(devRoot){
-        mount(devRoot)
-    }
+  if (devRoot) {
+    mount(devRoot);
+  }
 }
 
-export {mount}
+export { mount };
