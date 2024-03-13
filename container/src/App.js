0a443d08331f9377875b542354954917c3cb7f55
mehrnoush.navidi <mehr910093@gmail.com>
2024-03-13 11:06:56 +0330

    feat: add header component to container

--- container/src/App.js
+++ container/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+
+import { BrowserRouter } from "react-router-dom";
+
+import Header from "./component/Header";
 import MarketingApp from "./component/MarketingApp";
 
 export default () => {
   return (
-    <div>
-      <h1>hi from container</h1>;
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
