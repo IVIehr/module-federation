3611a7b36d8f91de16319e4e59cb765020676e83
mehrnoush.navidi <mehr910093@gmail.com>
2024-03-24 17:09:40 +0430

    feat: integrate dashboard with container

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import Header from "./component/Header";
 import AuthApp from "./component/AuthApp";
 import MarketingApp from "./component/MarketingApp";
+import DashboardApp from "./component/DashboardApp";
 
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
@@ -12,6 +13,7 @@ export default () => {
   return (
     <BrowserRouter>
       <div>
+        {isSignedIn && <Redirect to={"/dashboard"} />}
         <Header
           isSignedIn={isSignedIn}
           onSignOut={() => setIsSignedIn(false)}
@@ -19,6 +21,10 @@ export default () => {
         <Switch>
           <Route path={"/auth"}>
             <AuthApp onSignIn={() => setIsSignedIn(true)} />
+          </Route>
+          <Route path={"/dashboard"}>
+            {!isSignedIn && <Redirect to={"/"} />}
+            <DashboardApp />
           </Route>
           <Route path={"/"} component={MarketingApp} />
         </Switch>
